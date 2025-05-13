e4ab8632141ced0226b98688ca7a674c8ee7af8b
truongvck3313 <truongvck33@gmail.com>
2025-05-13 09:29:24 +0700

    12/05/2025

--- test_main_stx.py
+++ test_main_stx.py
@@ -21,9 +21,12 @@ class Test(unittest.TestCase):
         module_other_stx.clearData(var_stx.checklistpath, "Checklist", "", "", "")
         module_other_stx.clear_log()
         module_other_stx.delete_image()
-        module_stx.ModuleTest()
-        module_stx.retest_casenone(self)
-        module_stx.retest_casefail(self)
+        # module_stx.ModuleTest()
+        # module_stx.retest_casenone(self)
+        # module_stx.retest_casefail(self)
+        caseid_stx.caseid_minitor01(self)
+        caseid_stx.caseid_minitor02(self)
+
         try:
             module_other_stx.notification_telegram()
         except:

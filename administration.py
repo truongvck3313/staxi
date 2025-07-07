718bd5c440be2c07537355caa63de9e0978f2909
truongvck3313 <truongvck33@gmail.com>
2025-07-07 10:47:57 +0700

    [STAXI-32406]: test 1

--- administration.py
+++ administration.py
@@ -20,7 +20,7 @@ import subprocess
 import random
 wait = WebDriverWait(var_stx.driver, 10)
 
-#13232232332323232323
+#1322
 def get_info_web():
     var_stx.driver.implicitly_wait(0.05)
     row = 119

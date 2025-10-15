f05adcf11c0d6a3cf0844e8e64a599b0bb6ba6bb
truongvck3313 <truongvck33@gmail.com>
2025-10-15 16:38:44 +0700

    sai ở chỗ "Testing" có chữ hoa đầu, trong khi .lower() sẽ biến vế trái thành chữ thường hoàn toàn → so sánh sẽ luôn sai.

--- test_main_stx.py
+++ test_main_stx.py
@@ -19,7 +19,7 @@ import customer_stx
 
 class Test(unittest.TestCase):
     def test_run1(self):
-        print("d")
+        print("e")
         # module_other_stx.clearData(var_stx.checklistpath, "Checklist", "", "", "")
         # module_other_stx.clear_log()
         # module_other_stx.delete_image()

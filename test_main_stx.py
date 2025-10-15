ca06df4862b24e8214d5a133cb499c38151f0a7c
truongvck3313 <truongvck33@gmail.com>
2025-10-15 18:04:50 +0700

    [BAEX-18436]: truong test staging

--- test_main_stx.py
+++ test_main_stx.py
@@ -19,7 +19,7 @@ import customer_stx
 
 class Test(unittest.TestCase):
     def test_run1(self):
-        print("5")
+        print("6")
         # module_other_stx.clearData(var_stx.checklistpath, "Checklist", "", "", "")
         # module_other_stx.clear_log()
         # module_other_stx.delete_image()

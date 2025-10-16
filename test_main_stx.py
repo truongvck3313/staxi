96bcdfb2208fbefa1a4daca4fa87e2a0e5030585
truongvck3313 <truongvck33@gmail.com>
2025-10-16 13:34:49 +0700

    [BAEX-18465]: truong test

--- test_main_stx.py
+++ test_main_stx.py
@@ -19,7 +19,7 @@ import customer_stx
 
 class Test(unittest.TestCase):
     def test_run1(self):
-        print("n7")
+        print("n8")
         # module_other_stx.clearData(var_stx.checklistpath, "Checklist", "", "", "")
         # module_other_stx.clear_log()
         # module_other_stx.delete_image()

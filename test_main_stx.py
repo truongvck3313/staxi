5c94879a422f7aa714a3de54d4405001e5d7d320
truongvck3313 <truongvck33@gmail.com>
2025-12-03 11:41:18 +0700

    update 03/12 test1

--- test_main_stx.py
+++ test_main_stx.py
@@ -28,7 +28,7 @@ class Test(unittest.TestCase):
         module_stx.retest_casenone(self)
         module_stx.retest_casefail(self)
         module_other_stx.send_viber()
-        #abccca
+        #test1
 
 
 

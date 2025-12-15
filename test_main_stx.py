75520c6ba7aa1401ca8592f672402df767cdb31f
truongvck3313 <truongvck33@gmail.com>
2025-12-15 08:25:53 +0700

    đã sửa lại lỗi confict

--- test_main_stx.py
+++ test_main_stx.py
@@ -26,15 +26,15 @@ import customer_stx
 
 class Test(unittest.TestCase):
     def test_run1(self):
-        # module_other_stx.clearData(var_stx.checklistpath, "Checklist", "", "", "")
-        # module_other_stx.clear_log()
-        # module_other_stx.delete_image()
-        # module_other_stx.timerun()
-        # module_stx.ModuleTest()
-        # module_stx.retest_casenone(self)
-        # module_stx.retest_casefail(self)
-        # module_other_stx.send_viber()
-        #đã sửa lại lỗi chết luồng do khai báo driver
+        module_other_stx.clearData(var_stx.checklistpath, "Checklist", "", "", "")
+        module_other_stx.clear_log()
+        module_other_stx.delete_image()
+        module_other_stx.timerun()
+        module_stx.ModuleTest()
+        module_stx.retest_casenone(self)
+        module_stx.retest_casefail(self)
+        module_other_stx.send_viber()
+        # đã sửa lại lỗi confict
 
 
 

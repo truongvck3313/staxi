cf323124de5f02bbebae3904a51b64e00b80f485
truongvck3313 <truongvck33@gmail.com>
2026-01-13 08:55:06 +0700

    update staxi sáng 13/01/2026

--- test_main_stx.py
+++ test_main_stx.py
@@ -13,14 +13,14 @@ import module_stx
 #update staxi sáng 13/01/2026
 
 class Test(unittest.TestCase):
-    # def test_run1(self):
-    #     # module_other_stx.clearData(var_stx.checklistpath, "Checklist", "", "", "")
-    #     # module_other_stx.clear_log()
-    #     # module_other_stx.delete_image()
-    #     # module_other_stx.timerun()
-    #     # module_stx.ModuleTest()
-    #     # module_stx.retest_casenone(self)
-    #     # module_stx.retest_casefail(self)
+    def test_run1(self):
+        module_other_stx.clearData(var_stx.checklistpath, "Checklist", "", "", "")
+        module_other_stx.clear_log()
+        module_other_stx.delete_image()
+        module_other_stx.timerun()
+        module_stx.ModuleTest()
+        module_stx.retest_casenone(self)
+        module_stx.retest_casefail(self)
         module_other_stx.send_viber()
         #
         #

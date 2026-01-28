96ab6545fb79e496d0eaa1a8692fd01a9166452e
huonglt2 <huongltlt204@gmail.com>
2026-01-28 08:11:48 +0700

    update chieu 27/01

--- test_main_stx.py
+++ test_main_stx.py
@@ -10,27 +10,26 @@ import module_stx
 
 
 
-#update update chieu 27/011
+#update update sang 28/01
 
 
 class Test(unittest.TestCase):
 
     def test_run1(self):
-        module_other_stx.clearData(var_stx.checklistpath, "Checklist", "", "", "")
-        module_other_stx.clear_log()
-        module_other_stx.delete_image()
-        module_other_stx.timerun()
-        module_stx.ModuleTest()
-        module_stx.retest_casenone(self)
-        module_stx.retest_casefail(self)
-        module_other_stx.send_viber()
+        # module_other_stx.clearData(var_stx.checklistpath, "Checklist", "", "", "")
+        # module_other_stx.clear_log()
+        # module_other_stx.delete_image()
+        # module_other_stx.timerun()
+        # module_stx.ModuleTest()
+        # module_stx.retest_casenone(self)
+        # module_stx.retest_casefail(self)
+        # module_other_stx.send_viber()
         #
         #
         #
-        # caseid_stx.caseid_login01(self)
-
-        # # caseid_stx.caseid_login02(self)
-        # # caseid_stx.caseid_login03(self)
+        caseid_stx.caseid_login01(self)
+        caseid_stx.caseid_login02(self)
+        # caseid_stx.caseid_login03(self)
         #
         # # caseid_stx.caseid_login06(self)
         # caseid_stx.caseid_login07(self)
